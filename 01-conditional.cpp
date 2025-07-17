27c7f981b96c2eaa96de4a5f59317ca47ec7991b
soravsingh08 <rahulrakwal3@gmail.com>
2025-07-17 20:28:59 +0530

    Conditional Statements

---
+++ 01-conditional.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n =45;
+
+    if(n>7){
+        cout<<"n is positive\n";
+    }else {
+        cout<< "n is negative\n";
+    }
+
+    return 0;
+
+}
+
