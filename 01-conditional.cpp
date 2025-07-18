36a8f971b0c28ef35164752e12008bae4fdb375b
soravsingh08 <rahulrakwal3@gmail.com>
2025-07-18 20:37:17 +0530

    Conditional

--- 01-conditional.cpp
+++ 01-conditional.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-
+/*
 int main (){
     int n =45;
-
+ 
     if(n>7){
         cout<<"n is positive\n";
     }else {
@@ -14,3 +14,36 @@ int main (){
 
 }
 
+*/
+
+// FINDING EVEN NUMBER
+
+/*
+int main(){
+    int n;
+    cout << "enter number:" ;
+    cin >> n;
+
+    if(n%2 == 0){
+        cout << " number is even";
+
+    }else cout << "number is odd";
+}
+    */
+
+
+// gradign system
+
+int main(){
+    int n;
+    cout << "enter marks :";
+    cin >> n;
+
+    if(n>=90){
+        cout << "A" ;
+    }else if (n>= 80 && n <90){
+        cout << "B\n";
+    }else{
+     cout << "C\n";
+    }
+}
